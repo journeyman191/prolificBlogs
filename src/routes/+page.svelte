9eb3dd4c0b2978f33db264978fc274afff912f6d
Justin Garner <charlottewebsolutions1@gmail.com>
2023-10-18 15:06:45 -0400

    Base UI for admin to login and post blogs 95% done just need to add two fields, UI for blog homepage base done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,206 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav class="border-gray-200 bg-gray-50">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="#" class="flex items-center">
+          <span class="self-center text-2xl font-semibold whitespace-nowrap">"I'm out of town thuggin wit my rounds, my p..."🎶 </span>
+      </a>
+      <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-solid-bg" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          </svg>
+      </button>
+      <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+        <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent">
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About Us</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contact</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Services</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Item 4</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+
+<section class="bg-blue-200 text-gray-500">
+    <div class="container flex flex-col items-center px-5 py-24 mx-auto md:flex-row lg:px-24">
+      <div class="flex flex-col items-center mb-16 text-left lg:flex-grow lg:w-1/2 lg:pr-24 md:items-start md:mb-0">
+        <div class="flex flex-col w-full mb-8 lg:flex-grow md:items-start">
+          <span class="mb-4 text-sm font-semibold text-gray-600 uppercase dark:text-white">1600+ Subscribers</span>
+          <h1
+            class="mb-4 text-5xl font-bold leading-none tracking-tight text-black md:text-7xl 2xl:text-6xl lg:text-5xl title-font dark:text-gray-400">
+            A daily newsletter with fantatic content on all genres. From the one and only DKILLER982
+          </h1>
+        </div>
+        <div class="w-full">
+          <div class="emailoctopus-form-wrapper emailoctopus-form-unstyled null">
+            <p class="text-gray-500 emailoctopus-success-message"></p>
+            <p class="text-gray-500 emailoctopus-error-message"></p>
+            <form action="#" method="post" data-message-success="Thanks for subscribing!"
+              data-message-missing-email-address="Your email address is required."
+              data-message-invalid-email-address="Your email address looks incorrect, please try again."
+              data-message-bot-submission-error="This doesn't look like a human submission."
+              data-message-consent-required="Please check the checkbox to indicate your consent."
+              data-message-invalid-parameters-error="This form has missing or invalid fields."
+              data-message-unknown-error="Sorry, an unknown error has occurred. Please try again later."
+              class="items-center text-gray-500 emailoctopus-form" data-sitekey="#">
+              <div class="emailoctopus-form-row"></div>
+              <div aria-hidden="true" class="hidden emailoctopus-form-row-hp">
+                <input type="text" name="#" tabindex="-1" autocomplete="nope" />
+              </div>
+              <div class="text-center emailoctopus-form-row-subscribe">
+                <input
+                  class="w-full px-4 py-2 mb-4 text-base text-center text-gray-500 transition duration-500 ease-in-out transform bg-white border border-transparent border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                  id="field_0" name="field_0" type="email" placeholder="email@example.com" required="required" />
+                <input type="hidden" name="successRedirectUrl" />
+                <button type="submit"
+                  class="sub w-full px-4 py-2 text-lg tracking-wide text-gray-100 transition duration-500 ease-in-out transform bg-black border-blue-600 rounded-md">
+                  Subscribe
+                </button>
+              </div>
+            </form>
+          </div>
+          <script src="https://emailoctopus.com/bundles/emailoctopuslist/js/1.6/form-recaptcha.js"></script>
+          <script src="https://emailoctopus.com/bundles/emailoctopuslist/js/1.6/form-embed.js"></script>
+        </div>
+      </div>
+      <div class="hidden w-5/6 lg:max-w-lg lg:w-full md:w-1/2 lg:block">
+        <img class="object-cover object-center rounded" alt="hero"
+          src="https://d33wubrfki0l68.cloudfront.net/8a6d68f2c8f7121df4effaf66d0c742d0109ce64/afbdc/images/4.webp" />
+      </div>
+    </div>
+  </section>
+  <section class="text-gray-500 bg-blue-200">
+    <div class="w-full flex-grow sm:flex sm:items-center sm:w-auto mb-10">
+        <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
+            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a>
+            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Automotive</a>
+            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Finance</a>
+            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Politics</a>
+            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Culture</a>
+            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Sports</a>
+        </div>
+    </div>
+    <div class="container items-center px-5 pb-12 mx-auto lg:px-24">
+      <div class="flex flex-wrap -m-4">
+        <div class="w-full p-4 md:w-1/2 lg:w-1/3">
+          <div class="h-full overflow-hidden rounded-lg shadow-xl">
+            <img class="object-cover object-center w-full lg:h-48 md:h-36" src="https://images.pexels.com/photos/18770339/pexels-photo-18770339/free-photo-of-an-old-mosque-with-a-clock-tower-in-the-desert.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="unicornsfeed newsletter - Issue Nasd - asdf" />
+            <div class="content-around p-6">
+              <h2 class="mb-1 font-mono text-xs font-medium tracking-widest text-gray-400 title-font">
+                Science & Math
+              </h2>
+              <h3 class="mb-3 text-lg font-bold text-gray-900 title-font dark:text-white">
+                Why you get no bitches!!!
+              </h3>
+              <p class="mb-3 leading-relaxed lg:line-clamp-2 dark:text-gray-400">You don't get any bitches because you talk down to women.</p>
+              <div class="flex flex-wrap items-center">
+                <a href="./issue-n1.html"
+                  class="inline-flex items-center px-6 py-3 mt-4 text-lg font-semibold text-gray-600 transition duration-500 ease-in-out border rounded-lg hover:bg-gray-200 md:mb-2 lg:mb-0 hover:text-gray-900 dark:border-gray-600 dark:text-white dark:bg-gray-600">Read
+                  more &nbsp; »
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-full p-4 md:w-1/2 lg:w-1/3">
+            <div class="h-full overflow-hidden rounded-lg shadow-xl">
+              <img class="object-cover object-center w-full lg:h-48 md:h-36" src="https://images.pexels.com/photos/18770339/pexels-photo-18770339/free-photo-of-an-old-mosque-with-a-clock-tower-in-the-desert.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="unicornsfeed newsletter - Issue Nasd - asdf" />
+              <div class="content-around p-6">
+                <h2 class="mb-1 font-mono text-xs font-medium tracking-widest text-gray-400 title-font">
+                  Science & Math
+                </h2>
+                <h3 class="mb-3 text-lg font-bold text-gray-900 title-font dark:text-white">
+                  Why you get no bitches!!!
+                </h3>
+                <p class="mb-3 leading-relaxed lg:line-clamp-2 dark:text-gray-400">You don't get any bitches because you talk down to women.</p>
+                <div class="flex flex-wrap items-center">
+                  <a href="./issue-n1.html"
+                    class="inline-flex items-center px-6 py-3 mt-4 text-lg font-semibold text-gray-600 transition duration-500 ease-in-out border rounded-lg hover:bg-gray-200 md:mb-2 lg:mb-0 hover:text-gray-900 dark:border-gray-600 dark:text-white dark:bg-gray-600">Read
+                    more &nbsp; »
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="w-full p-4 md:w-1/2 lg:w-1/3">
+            <div class="h-full overflow-hidden rounded-lg shadow-xl">
+              <img class="object-cover object-center w-full lg:h-48 md:h-36" src="https://images.pexels.com/photos/18770339/pexels-photo-18770339/free-photo-of-an-old-mosque-with-a-clock-tower-in-the-desert.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="unicornsfeed newsletter - Issue Nasd - asdf" />
+              <div class="content-around p-6">
+                <h2 class="mb-1 font-mono text-xs font-medium tracking-widest text-gray-400 title-font">
+                  Science & Math
+                </h2>
+                <h3 class="mb-3 text-lg font-bold text-gray-900 title-font dark:text-white">
+                  Why you get no bitches!!!
+                </h3>
+                <p class="mb-3 leading-relaxed lg:line-clamp-2 dark:text-gray-400">You don't get any bitches because you talk down to women.</p>
+                <div class="flex flex-wrap items-center">
+                  <a href="./issue-n1.html"
+                    class="inline-flex items-center px-6 py-3 mt-4 text-lg font-semibold text-gray-600 transition duration-500 ease-in-out border rounded-lg hover:bg-gray-200 md:mb-2 lg:mb-0 hover:text-gray-900 dark:border-gray-600 dark:text-white dark:bg-gray-600">Read
+                    more &nbsp; »
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="w-full p-4 md:w-1/2 lg:w-1/3">
+            <div class="h-full overflow-hidden rounded-lg shadow-xl">
+              <img class="object-cover object-center w-full lg:h-48 md:h-36" src="https://images.pexels.com/photos/18770339/pexels-photo-18770339/free-photo-of-an-old-mosque-with-a-clock-tower-in-the-desert.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="unicornsfeed newsletter - Issue Nasd - asdf" />
+              <div class="content-around p-6">
+                <h2 class="mb-1 font-mono text-xs font-medium tracking-widest text-gray-400 title-font">
+                  Science & Math
+                </h2>
+                <h3 class="mb-3 text-lg font-bold text-gray-900 title-font dark:text-white">
+                  Why you get no bitches!!!
+                </h3>
+                <p class="mb-3 leading-relaxed lg:line-clamp-2 dark:text-gray-400">You don't get any bitches because you talk down to women.</p>
+                <div class="flex flex-wrap items-center">
+                  <a href="./issue-n1.html"
+                    class="inline-flex items-center px-6 py-3 mt-4 text-lg font-semibold text-gray-600 transition duration-500 ease-in-out border rounded-lg hover:bg-gray-200 md:mb-2 lg:mb-0 hover:text-gray-900 dark:border-gray-600 dark:text-white dark:bg-gray-600">Read
+                    more &nbsp; »
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="w-full p-4 md:w-1/2 lg:w-1/3">
+            <div class="h-full overflow-hidden rounded-lg shadow-xl">
+              <img class="object-cover object-center w-full lg:h-48 md:h-36" src="https://images.pexels.com/photos/18770339/pexels-photo-18770339/free-photo-of-an-old-mosque-with-a-clock-tower-in-the-desert.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="unicornsfeed newsletter - Issue Nasd - asdf" />
+              <div class="content-around p-6">
+                <h2 class="mb-1 font-mono text-xs font-medium tracking-widest text-gray-400 title-font">
+                  Science & Math
+                </h2>
+                <h3 class="mb-3 text-lg font-bold text-gray-900 title-font dark:text-white">
+                  Why you get no bitches!!!
+                </h3>
+                <p class="mb-3 leading-relaxed lg:line-clamp-2 dark:text-gray-400">You don't get any bitches because you talk down to women.</p>
+                <div class="flex flex-wrap items-center">
+                  <a href="./issue-n1.html"
+                    class="inline-flex items-center px-6 py-3 mt-4 text-lg font-semibold text-gray-600 transition duration-500 ease-in-out border rounded-lg hover:bg-gray-200 md:mb-2 lg:mb-0 hover:text-gray-900 dark:border-gray-600 dark:text-white dark:bg-gray-600">Read
+                    more &nbsp; »
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </section>
+
+  <style>
+    .sub{
+        background-color: black;
+    }
+  </style>
---
+++ src/routes/post/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    import Editor from "@tinymce/tinymce-svelte";
+
+    let conf = {
+        content_style: ".p { color: red; } body { margin: 0; padding: 0; }",
+        height: "100vh",
+    }
+</script>
+
+
+<div class="w-screen h-screen flex flex-wrap">
+    <div class="w-full lg:w-1/2">
+        <div class="mt-16 md:mt-28 lg:mt-40 flex flex-col justify-center items-center mb-10">
+            <h1 class="mb-1 md:mx-0 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Post Blog</h1>
+            <p class="text-gray-600 text-center">Blogs posted today: 43</p>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+            <div class="w-2/3 mb-4">
+                <label for="blogTitle" class="block text-sm font-medium leading-6 text-gray-900">What is the blog title?</label>
+                <div class="relative mt-2 rounded-md shadow-sm">
+                    <input type="text" name="blogTitle" id="blogTitle" class="text-sm block w-full rounded-md border-0 py-2.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6" placeholder="Fun time with friends">
+                </div>
+            </div>
+        
+            <div class="w-2/3 mb-6 flex flex-col">
+                <label for="keywords" class="block text-sm font-medium leading-6 text-gray-900">What keywords would you like to add?</label>
+                <div class="relative mt-2 mb-6 rounded-md shadow-sm">
+                    <input type="text" name="keywords" id="keywords" class="text-sm block w-full rounded-md border-0 py-2.5 pl-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6" placeholder="Fun time with friends">
+                </div>
+                <button class="justify-self-end bg-black text-white transform p-4 rounded-xl self-end font-semibold">Next</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="w-full lg:w-1/2 px-10 pt-10">
+        <div class="rounded-md shadow-sm">
+            <Editor apiKey="6bonds4k9gcv6r9duxe2mgtgf6zovqhix4qglxzkkppws98h" {conf}/>
+        </div>
+    </div>
+</div>
