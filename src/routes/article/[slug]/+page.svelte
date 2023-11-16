21e49ee1a413c0c215369e050ec14e2775345be9
Justin Garner <charlottewebsolutions1@gmail.com>
2023-11-15 21:42:01 -0500

    Changed Blogger Name and Blogger Pic

--- src/routes/article/[slug]/+page.svelte
+++ src/routes/article/[slug]/+page.svelte
@@ -158,10 +158,10 @@
             <header class="mb-4 lg:mb-6">
                 <address class="flex items-center mb-6">
                     <div class="inline-flex items-center mr-3 text-sm text-gray-900">
-                        <img class="w-16 h-16 aspect-auto rounded-full" src={icon} alt="Jese Leos">
+                        <img class="w-16 h-16 aspect-auto rounded-full" src="https://image.mymixtapez.com/albums/217977/cover/0/large" alt="Jese Leos">
                         <div class="ml-4">
-                            <a href="#" rel="author" class="text-xl font-bold text-gray-900">Dillan Garner</a>
-                            <p class="text-base text-gray-500">CPCC Student, public speaker & tutor</p>
+                            <a href="#" rel="author" class="text-xl font-bold text-gray-900">DonQuavious Jamal LaJackson</a>
+                            <p class="text-base text-gray-500">College Student, public speaker & tutor</p>
                             <p class="text-base text-gray-500"><time pubdate datetime="2022-02-08" title="February 8th, 2022">{formatTimestamp(data.timestamp)}</time></p>
                         </div>
                     </div>
